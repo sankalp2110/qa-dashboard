20e2efb17e6bc8088aad9bd36664b6ec70e0c53f
Sankalp Nihal Pandey <sankalpnihalpandey@Sankalps-MacBook-Air.local>
2023-07-06 21:25:20 +0530

    changed the API key

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -46,7 +46,7 @@ export default function Login() {
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
-          viewBox="0 0 20 24"
+          viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
           strokeWidth="2"
