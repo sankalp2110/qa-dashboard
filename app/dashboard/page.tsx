6557ea4186eea83ad55ceb513b4d9b1338073964
sujithkp <pendem.s@geekyants.com>
2023-07-17 16:08:10 +0530

    Fixed Navigation bar issue in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,14 +2,20 @@
 import React from 'react';
 
 import NavigationBar from '@/components/NavigationBar';
+import Footer from '@/components/Footer';
+import { VerticalFeatures } from '@/components/VerticalFeatures';
 
 export default async function Dashboard(){
     return (
+        
         <div className='w-full items-center'>
-            <NavigationBar/>
-            Hello
-
+            <nav className="w-full justify-center border-b border-b-foreground/10 h-16 ">
+                <NavigationBar/>
+            </nav>
+            
         </div>
+        
+    
       );
 
   
